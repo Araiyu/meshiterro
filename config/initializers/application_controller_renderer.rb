906b2eccd103e590952b616e49523bde65237a88
新井　雄介 <[ay088055@gmail.com]>
2019-06-12 16:00:45 +0900

    F-画像投稿

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,4 @@
 #     https: false
 #   )
 # end
+Refile.secret_key = 'bf59a9676f69e4fcd2dc09488e3a77a381ad63ba94095eebf6fbe58c8ade8feb13629f041c5d88be5609c082e5bb8d1f634198b75deb80c9b0b1260f178c6c43'
